4db72ead069e5247c7268d57ecacb0850c3f6af3
Dan <rusnac14@gmail.com>
2020-09-06 16:09:11 +0200

    Seed user and portfolio

---
+++ database/seeds/1.InitialSeeder.js
@@ -0,0 +1,48 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| InitialSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+const User = use('App/Models/User')
+const Database = use('Database')
+const uuid = require("uuid/v4")
+const Hash = use('Hash')
+
+class InitialSeeder {
+    async run() {
+        /**
+         * Seeding Users
+         */
+        console.log('Seeding Users')
+        await Database.table('users')
+            .insert({
+                username: 'testUser'
+            })
+        const user = await User.first()
+
+        /**
+         * Seeding Portfolios
+         */
+        console.log('Seeding Portfolios')
+        await Database.table('portfolios')
+            .insert({
+                user_id: user.id,
+                slug: 'testPortfolio',
+                description: 'Test portfolio with only read permissions',
+                api_key: '2uQAZvjbwwrwqG3Qqbx3XCB43LxRaQw85lPSuoyXGilVfAdfnh2F0mTRjrb7UP4s',
+                api_secret:  await Hash.make('sqejRQMHIbbN316cJElzVo758yWyi2KPu8cWVvT0tTKGA2wv511ibd3v6WbVDk6W')
+            })
+
+    }
+}
+
+module.exports = InitialSeeder
